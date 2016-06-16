626ec8ae60d3b1894e40c9621925ebc217f6b016
tss <tss@debian-tss24>
2016-06-16 21:37:48 +0200

    16/06 - 02

--- src/main/java/it/java858/todoapp/entity/Categoria.java
+++ src/main/java/it/java858/todoapp/entity/Categoria.java
@@ -63,8 +63,14 @@ public class Categoria implements Serializable {
         }
         return true;
     }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "id=" + id + ", nome=" + nome + '}';
+    }
+
     
-    
+     
     
     
 }
